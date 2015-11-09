1dbca0a0833338c7f00c663e0e11bb6e3b84f9ab
Farteen <xujavacn@gmail.com>
2015-11-09 23:13:03 +0800

    add setup.py

---
+++ C1/setup.py
@@ -0,0 +1,12 @@
+from dsitutils.core import setup
+
+setup {
+	name 		=	'nester',
+	version 	=	'1.0.0',
+	py_moduls	= 	['nester'],
+	author		= 	'farteen',
+	author_email	=	'934628017@qq.com',
+	url		=	'http://github.com/farteen',
+	description	=	'A simple printer'
+}
+
