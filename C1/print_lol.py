e52c8e072f9f901989d1a310375a266cf9fa3ae5
Farteen <xujavacn@gmail.com>
2015-11-11 20:36:38 +0800

    add print_lol.py

---
+++ C1/print_lol.py
@@ -0,0 +1,10 @@
+def print_lol(the_list, level):
+	for each_item in the_list:
+		if isinstance(each_item, list):
+			print_lol(each_item)
+		else:
+			for tab_stop in range(level):
+				print("\t",end='')
+			print(each_item)
+
+print_lol([1,2,3],2)
